3951e1749c479c83868b29d1e67f0da09c25c76b
Paulo Stanize <paulostanize@gmail.com>
2019-09-27 18:41:53 -0300

    setup project with sass

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -34,13 +34,13 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
 h3 {
   margin: 40px 0 0;
 }
@@ -54,5 +54,9 @@ li {
 }
 a {
   color: #42b983;
+}
+
+h1 {
+  background: $primary;
 }
 </style>
